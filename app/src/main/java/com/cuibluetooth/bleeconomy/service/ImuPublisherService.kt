5d9a87670a968a62da5ce0b26ffdd6bbe0250da1
Cristop1 <capu.lot.quispe@gmail.com>
2025-10-06 03:00:40 -0500

    MQTT publisher 1st try implementation, yet to implement: - Connection to AWS or Azure (Login) - Mqtt subscriber (position + people batch) to IOT Core broker - When getting the batches, update the UI accordingly.

--- app/src/main/java/com/cuibluetooth/bleeconomy/service/ImuPublisherService.kt
+++ app/src/main/java/com/cuibluetooth/bleeconomy/service/ImuPublisherService.kt
@@ -37,6 +37,7 @@ class ImuPublisherService : LifecycleService() {
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        super.onStartCommand(intent, flags, startId)
         return START_STICKY
     }
 
