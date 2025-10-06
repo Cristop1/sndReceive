cbb8c489f899195088a54e5c615a2a018bbc9eb7
Cristop1 <capu.lot.quispe@gmail.com>
2025-10-06 16:39:25 -0500

    Rework Branch

---
+++ app/src/main/java/com/cuibluetooth/bleeconomy/LaunchActivity.kt
@@ -0,0 +1,13 @@
+package com.cuibluetooth.bleeconomy
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+@SuppressLint("CustomSplashScreen")
+class LaunchActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
